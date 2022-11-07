10610da7e82db17cfd77eb9e040d99a2d4c221d3
net <wendell20904102@gmail.com>
2022-11-07 09:08:15 +0800

    make section layout

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@ import { globalLoading } from '@/store/common.js'
 import GlobalLoading from '@/components/business/GlobalLoading.vue'
 import NavBar from '@/components/business/NavBar.vue'
 import SiteBanner from '@/components/business/SiteBanner.vue'
+import SectionReview from '@/components/business/SectionReview.vue'
+import SectionReward from '@/components/business/SectionReward.vue'
+import SectionFaq from '@/components/business/SectionFaq.vue'
+import DiamondSponser from '@/components/business/DiamondSponser.vue'
+import VButton from '@/components/business/VButton.vue'
+import { delayNavigate } from '@/utils/browser.js'
 import { useWindowSize } from '@vueuse/core'
 import { computed, ref } from 'vue'
 
@@ -14,11 +20,46 @@ const siteBannerHeight = computed(() => windowHeight.value - navBarHeight.value)
 <template>
   <NavBar :height="navBarHeight" />
   <SiteBanner :height="siteBannerHeight" />
+  <SectionReview class="layout-section-review" />
+  <SectionReward class="layout-section-reward" />
+  <SectionFaq class="layout-section-faq" />
+  <div class="layout-section-registration">
+    <div class="container">
+      <VButton
+        size="expand"
+        flick
+        @click="delayNavigate('https://2022.thef2e.com/signup')"
+        >立即註冊報名!</VButton
+      >
+    </div>
+  </div>
+
+  <DiamondSponser />
   <GlobalLoading :show="globalLoading" />
 </template>
 
 <style lang="scss">
-#app {
-  height: 300vh;
+.layout {
+  &-section-review {
+    margin-bottom: 100px;
+    @include breakpoint('tablet') {
+      margin-bottom: 223px;
+    }
+  }
+  &-section-reward {
+    margin-bottom: 200px;
+    @include breakpoint('tablet') {
+      margin-bottom: 534px;
+    }
+  }
+  &-section-faq {
+    margin-bottom: 200px;
+    @include breakpoint('tablet') {
+      margin-bottom: 250px;
+    }
+  }
+  &-section-registration {
+    margin-bottom: 200px;
+  }
 }
 </style>
